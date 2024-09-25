ddff4e457d0e93f8e332b6a49297864b05f51069
Pablo Scherpa <desarrollador@pabloscherpa.com.ar>
2024-09-24 22:41:30 -0300

    create controllers

---
+++ src/controllers/ProjectController.ts
@@ -0,0 +1,32 @@
+import { Request, Response } from "express";
+import Project from "../models/Project";
+
+export class ProjectController {
+  static createProject = async (req: Request, res:Response) => {
+    const project = new Project(req.body)
+    try {
+        await project.save()
+        res.send('Proyecto Creado Correctamente')
+    } catch (error) {
+        console.log(error)
+    }
+  };
+
+  static getAllProjects = async (req: Request, res:Response) => {
+    try {
+        const projects = await Project.find({})
+        res.json(projects)
+    } catch (error) {
+        console.log(error)
+    }
+  };
+
+  static getProjectById = async (req: Request, res: Response) => {
+    try {
+        const project = await Project.findById({})
+        res.json(project)
+    } catch (error) {
+        console.log(error)
+    }
+  }
+}
---
+++ src/routes/projectRoutes.ts
@@ -0,0 +1,27 @@
+import { Router } from "express";
+import { body } from "express-validator";
+import { ProjectController } from "../controllers/ProjectController";
+import { handleInputErrors } from "../middleware/validation";
+
+const router = Router();
+
+//POST
+router.post(
+  "/",
+  body("projectName")
+    .notEmpty()
+    .withMessage("El nombre del proyecto es obligatorio"),
+  body("clientName")
+    .notEmpty()
+    .withMessage("El nombre del cliente es obligatorio"),
+  body("description")
+    .notEmpty()
+    .withMessage("La descripcion del proyecto es obligatoria"),
+  handleInputErrors,
+  ProjectController.createProject
+);
+
+//GET
+router.get("/", ProjectController.getAllProjects);
+
+export default router;
