a93a64eeb67a9579da64ad175017e20002fabe2d
Pablo Scherpa <desarrollador@pabloscherpa.com.ar>
2024-09-25 08:39:00 -0300

    creating task status

--- src/models/Task.ts
+++ src/models/Task.ts
@@ -1,9 +1,20 @@
 import mongoose, { Schema, Document, Types } from "mongoose";
 
+const taskStatus = {
+    PENDING: "pending",
+    ON_HOLD: "onHold",
+    IN_PROGRESS: "inProgress",
+    UNDER_REVIEW: "underReview",
+    COMPLETED: "completed",
+} as const
+
+export type TaskStatus = (typeof taskStatus)[keyof typeof taskStatus]
+
 export interface ITask extends Document {
   name: string;
   description: string;
   project: Types.ObjectId;
+  status: TaskStatus;
 }
 
 export const TaskSchema: Schema = new Schema(
@@ -21,6 +32,11 @@ export const TaskSchema: Schema = new Schema(
     project: {
       type: Schema.Types.ObjectId,
       ref: "Project",
+    },
+    status: {
+      type: String,
+      enum: Object.values(taskStatus),
+      default: taskStatus.PENDING,
     },
   },
   { timestamps: true }
